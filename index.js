051e4514ce7f494c789c53457b982b73f22a5249
James Perih <james.perih@brewninja.net>
2020-01-15 18:10:07 -0600

    Fix restaurant query to new graphql syntax.

--- index.js
+++ index.js
@@ -25,7 +25,9 @@ if (argv.length > 3) {
 }
 
 const searchPhrase = argv[2];
-const RESTAURANT_URL = `https://api.skipthedishes.com/customer/v1/restaurants/search/web?language=en&lat=${LAT}&long=${LNG}&order_type=DELIVERY`;
+const search = searchPhrase;
+
+const RESTAURANT_URL = `https://api.skipthedishes.com/customer/v1/graphql?operationName=QueryRestaurantsList&variables=%7B%22latitude%22%3A${LAT}%2C%22longitude%22%3A${LNG}%2C%22isDelivery%22%3Atrue%2C%22search%22%3A%22%22%2C%22dateTime%22%3A0%2C%22language%22%3A%22en%22%7D&extensions=%7B%22persistedQuery%22%3A%7B%22version%22%3A1%2C%22sha256Hash%22%3A%229ec5f77aba912fb31aba9fa3a4092ebad2bed55ffe36404787f0e368e4a140f7%22%7D%7D`;
 const MENU_URL = `https://api-skipthedishes.skipthedishes.com/v1/restaurants/clean-url/:cleanUrl?fullMenu=true&language=en&lat=${LAT}&long=${LNG}&order_type=DELIVERY`
 const HEADERS = {
     'App-Token': APP_TOKEN,
@@ -33,47 +35,66 @@ const HEADERS = {
 };
 
 console.info(`Searching for [${searchPhrase}]...\n`);
+const results = [];
 
-fetch(RESTAURANT_URL, {
-    headers: {
-        ...HEADERS,
-    },
-}).then((response) => {
-    return response.json();
-}).then((parsedResponse) => {
-    const { restaurants } = parsedResponse;
-    restaurants.forEach((restaurant) => {
+const doProcessing = async () => {
+    const restaurantResponse = await fetch(RESTAURANT_URL, {
+        headers: {
+            ...HEADERS,
+        },
+    });
+
+    const parsedResponse = await restaurantResponse.json();
+    const { data: { restaurantsList: { openRestaurants }} } = parsedResponse;
+
+    if (openRestaurants.length === 0) {
+        console.log('No restaurants');
+        // res.status(404).json({ status: `no restaurants with params search: [${search}], lat: [${lat}], lng: [${lng}]`});
+        // res.end();
+    } else {
+        console.log(`fetched ${openRestaurants.length} restaurants.`);
+    }
+
+    // for each restaurant, get the menu items
+    for (let restaurant of openRestaurants) {
         const { name: restaurantName, cleanUrl } = restaurant;
-        fetch(MENU_URL.replace(':cleanUrl', cleanUrl), {
+
+        console.log(`getting ${cleanUrl}`);
+
+        const menuResponse = await fetch(MENU_URL.replace(':cleanUrl', cleanUrl), {
             headers: {
                 ...HEADERS,
-            },
-        }).then((menuResponse) => {
-            return menuResponse.json();
-        }).then((parsedMenuResponse) => {
-            const { menu: { menuGroups } } = parsedMenuResponse;
-            menuGroups.forEach((menuGroup) => {
-                if (menuGroup.menuItems.length > 0) {
-                    menuGroup.menuItems.forEach((menuItem) => {
-                        const { name, description, centsPrice, available } = menuItem;
-
-                        if (name.indexOf(searchPhrase) > -1 || description.indexOf(searchPhrase) > -1) {
-                            if (available) {
-                                console.log(`Matching item from ${restaurantName} (https://www.skipthedishes.com/${cleanUrl})`);
-                                console.log(`    Menu Item: ${name}`);
-                                console.log(`    Price: $${centsPrice / 100}`);
-                                console.log(`    Description: ${description}\n`);
-                            } else {
-                                console.log(menuItem);
-                            }
-                        }
-                    });
-                }
-            });
-        }).catch((menuError) => {
-           console.log('Menu error:', menuError);
+            }
         });
-    });
-}).catch((err) => {
-    console.error('Error: ', err);
-});
+        const parsedMenuResponse = await menuResponse.json();
+
+        const { menu: { menuGroups } } = parsedMenuResponse;
+        for (let menuGroup of menuGroups) {
+            if (menuGroup.menuItems.length > 0) {
+                for (let menuItem of menuGroup.menuItems) {
+                    const { name, description, centsPrice, available } = menuItem;
+
+                    if (name.indexOf(search) > -1 || description.indexOf(search) > -1) {
+                        if (available) {
+                        results.push({
+                            restaurantName,
+                            url: `https://www.skipthedishes.com/${cleanUrl}`,
+                            name,
+                            price: centsPrice / 100,
+                            description,
+                        });
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+    console.log(results);
+};
+
+doProcessing();
+console.log('end of function');
+//   res.status(200).json(results);
+//   res.end();
